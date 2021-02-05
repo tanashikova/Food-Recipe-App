1084bc271350ac087dfc96399438144f3342c741
didin21 <tanashikovadina@gmail.com>
2021-02-04 22:09:06 -0800

    final corrections and updated readme file

--- js/script.js
+++ js/script.js
@@ -1,62 +1,48 @@
 const searchBtn = document.querySelector("#btn");
 let results = document.querySelector(".search-result");
-const watchVideoBtn = document.querySelector(".watch-video-btn");
 const boxModel = document.querySelector(".content");
 const closeBtn = document.querySelector(".close-btn");
 
 searchBtn.addEventListener("click", (event) => {
   let search = document.querySelector("#search").value;
   fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${search}`)
-    .then((resData) => {
-      return resData.json();
-    })
-    .then((parsedData) => {
-      printResult(parsedData);
-    })
-    .catch((error) => {
-      console.log("error!!!:", error);
-    });
+    .then((resData) => resData.json())
+    .then((parsedData) => printResult(parsedData))
+    .catch((error) => console.log("error!!!:", error));
 });
 
 const printResult = (parsedData) => {
   let result = "";
   if (parsedData.meals) {
     parsedData.meals.forEach((meal) => {
-      result += `<div class="item" data-id =${meal.idMeal} >
-     
-      <img src="${meal.strMealThumb}">
-      <div class="title-container">
-        <h1 class="title">${meal.strMeal}</h1>
-        <button class="see-details-btn">See details</button>
-     
-    </div>
-    </div>`;
+      result += `<div class="card" data-id =${meal.idMeal} >
+                 <img src="${meal.strMealThumb}">
+                <div class="title-container">
+                <h1 class="title">${meal.strMeal}</h1>
+                 <button class="see-details-btn">See details</button>
+                </div>
+                </div>`;
     });
-    results.classList.remove("noResults");
   } else {
     result = "Sorry, there is no meal for your search";
-    results.classList.add("noResults");
   }
-
-  results.innerHTML = result;
-  results.parentElement.classList.add("display-search-result");
+    results.innerHTML = result;
+  results.parentNode.classList.add("display-search-result");
 };
 
-const fetchMealDetails = (e) => {
-  e.preventDefault();
-  let mealPrep = e.target.parentElement.parentElement;
-  fetch(
-    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealPrep.dataset.id}`
+results.addEventListener("click", (event) => {
+  let mealDetails = event.target.parentNode.parentNode;
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealDetails.dataset.id}`
   )
-    .then((response) => response.json())
-    .then((data) => openBoxModel(data.meals));
-};
+    .then((resData) => resData.json())
+    .then((parsedData) => openBoxModel(parsedData.meals))
+    .catch((error) => console.log("error!!!:", error));
+});
 
 const openBoxModel = (meal) => {
-    let modalBoxUI = "";
-  console.log(meal);
+  let modalBoxUI = "";
   meal = meal[0];
-   modalBoxUI += `
+  modalBoxUI += `
         <ul class = "ingridiens">  Main Ingredients
             <li> ${meal.strIngredient1}</li> 
             <li> ${meal.strIngredient2}</li>
@@ -64,16 +50,15 @@ const openBoxModel = (meal) => {
             <li> ${meal.strIngredient4}</li>
             <li> ${meal.strIngredient5}</li>
          </ul>
-        <div class = "recipe-link">
+        <div class = "link">
             <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
         </div>
  
     `;
   boxModel.innerHTML = modalBoxUI;
-  boxModel.parentElement.classList.add("details");
+  boxModel.parentNode.classList.add("details");
 };
 
-results.addEventListener("click", fetchMealDetails);
-closeBtn.addEventListener("click", () => {
-  boxModel.parentElement.classList.remove("details");
-});
+closeBtn.addEventListener("click", () =>
+  boxModel.parentNode.classList.remove("details")
+);
